7fab7583b2dbb830268bc52921d475a0e0a27ca2
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2022-12-15 15:00:12 +0100

    Mark Item as purchased

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,9 +15,25 @@ class ItemsController < ApplicationController
   end
 
   def update
+    # params["item"][:purchased]
+    # item_params[:purchased]
+    # purchased checked   "purchased"=>"1", "name"=>"item to list id 11", "amount"=>"1", "comment"=>"??"}, "commit"=>"Update Item", "id"=>"3"
+    # purchased UNchecked "purchased"=>"0", "name"=>"item to list id 11", "amount"=>"1", "comment"=>"??"}, "commit"=>"Update Item", "id"=>"3"
+    # purchased no info   "purchased"=>"0", "name"=>"",                   "amount"=>"",  "comment"=>""},   "commit"=>"Update Item", "id"=>"3"
+
     @item = Item.find(params[:id])
 
     authorize @item
+
+    if item_params[:purchased] == '1'
+      unless @item.user == current_user
+        @item.user = current_user
+        @item.purchased_date = DateTime.now
+      end
+    else
+      @item.user = nil
+      @item.purchased_date = nil
+    end
 
     @list = @item.list
 
@@ -44,6 +60,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :amount, :comment)
+    params.require(:item).permit(:name, :amount, :comment, :purchased)
   end
 end
