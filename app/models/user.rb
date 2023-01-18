25c2077f9d09706f106a6ca33d80236865bfb88a
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2023-01-18 17:26:22 +0100

    Callback after destroying User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_destroy :clean_user_records
+
   has_one_attached :avatar
   has_many :user_lists, dependent: :destroy # User.find(id).user_lists
   has_many :lists, through: :user_lists # User.find(id).lists
@@ -19,6 +21,24 @@ class User < ApplicationRecord
       errors.add(:avatar, ' should be less than 1MB') if avatar.byte_size > 1048576
       errors.add(:avatar, ' format should be jpeg, jpg or png') if !avatar.content_type.in?(%w[image/jpeg image/jpg image/png])
       # raise
+    end
+  end
+
+  private
+
+  def clean_user_records
+    done_items = Item.where(user: self)
+    done_items.each do |item|
+      item.user = nil
+      item.purchased_date = nil
+      item.purchased = false
+      item.save
+    end
+
+    lists_created_by_user = UserList.where(user: self, admin: true)
+    lists_created_by_user.each do |user_list|
+      user_list.destroy
+      user_list.list.destroy
     end
   end
 end
