6ff73c5ca192b92500521fe25359bfdae7118f4b
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2023-02-17 18:38:06 +0100

    Add test helper to display detailed info of test (yes, I know about the existence of test -v)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,21 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+module TestInfoHelper
+  def mark_test_start_time
+    @start_time = Time.now
+  end
+
+  def record_test_duration
+    puts ""
+    puts ""
+    puts "Test class:   #{self.class.name}"
+    puts "Test method:  #{self.method_name}"
+    puts "Duration:     #{Time.now - @start_time}"
+  end
+end
+
+class ActiveSupport::TestCase
+  include TestInfoHelper
+end
