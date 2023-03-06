5e0f6d93c038c07225b5c5bdd795bad3a5e54d03
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2023-03-06 20:42:46 +0100

    Debugging test workflow GHA 7

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,17 +2,7 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    p root_url
-    p root_path
     get root_path
-    # p response.body
-    assert_response :success
-  end
-
-  test "should get home II" do
-    get root_path
-    sleep 0.1
-    # p response.body
     assert_response :success
   end
 end
