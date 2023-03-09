2405b5643fe5ec919fe3a05f2261bf8ca472308e
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2023-03-09 17:30:01 +0100

    Set cloudinary and set host name in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -67,7 +67,7 @@ Rails.application.configure do
   # action_mailer settings
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: "tbd" }
+  config.action_mailer.default_url_options = { host: "in-the-list.up.railway.app" }
 
   config.action_mailer.perform_caching = false
 
