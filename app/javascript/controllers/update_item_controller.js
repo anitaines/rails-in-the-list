026ca11f252e65983f7cb6aa6e9dd77b6e87220d
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2023-03-13 18:31:03 +0100

    Add function to set item inactive in update item js controller

--- app/javascript/controllers/update_item_controller.js
+++ app/javascript/controllers/update_item_controller.js
@@ -25,6 +25,52 @@ export default class extends Controller {
       })
   }
 
+  inactiveItem(event) {
+    event.preventDefault();
+
+    const itemName = this.formTarget.dataset.name;
+
+    swal({
+      text: `Hide ${itemName}? You'll still be able to find it on the Usual items tab`,
+      icon: "warning",
+      buttons: ["No", "Yes"],
+      dangerMode: true,
+    })
+    .then((willHide) => {
+      if (willHide) {
+
+        this.updatingTarget.classList.remove('d-none');
+
+        const url = this.formTarget.action;
+
+        fetch(url, {
+          method: "PATCH",
+          headers: { "Accept": "text/plain" },
+          body: new FormData(this.formTarget)
+        })
+        .then(response => response.text())
+        .then((data) => {
+          this.element.remove();
+
+          const items = document.querySelectorAll('.item');
+          if (items.length == 0){
+            document.querySelector('.list .empty').classList.remove('d-none');
+          }
+        })
+        .catch(err => {
+          if (err) {
+            swal("Oh noes!", "Something went wrong, please refresh and try again", "error");
+          } else {
+            swal.stopLoading();
+            swal.close();
+          }
+        });
+      } else {
+        event.target.checked = true;
+      }
+    });
+  }
+
   deleteItem(event) {
     event.preventDefault();
     const itemName = this.deleteFormTarget.dataset.name;
