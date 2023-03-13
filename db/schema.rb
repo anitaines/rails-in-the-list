fa2b9166bd399dbcb5450474a6f107be709dfd8c
Ana Ines Guagliardi <gdi.anaines@gmail.com>
2023-03-13 18:13:45 +0100

    Add column Active to Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_171810) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_12_124509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_171810) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: true
     t.index ["list_id"], name: "index_items_on_list_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
